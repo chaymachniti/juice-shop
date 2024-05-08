883ed39bc0ca3a507348fef6cb20da0243217992
cchniti <chniti.chayma@gmail.com>
2024-05-08 10:57:35 +0100

    test pipline

--- scripts/send_scans.py
+++ scripts/send_scans.py
@@ -147,7 +147,7 @@ def main():
   ENGAGEMENT_DURATION_DAYS = 100  # Medium Finding SLA Days + 10
   send_scans.create_engagement(PIPELINE_ID, COMMIT_HASH, BRANCH_OR_TAG, VERSION, REPO_URI, SCM_SERVER, BUILD_SERVER, ENGAGEMENT_DURATION_DAYS)
   scans = [
-  {"scan_type": "AuditJS Scan ", "scan_file": "audit-report.json"},
+  {"scan_type": "ZAP Scan ", "scan_file": "report_xml.xml"},
   ]
   send_scans.upload_scans(scans)
 
